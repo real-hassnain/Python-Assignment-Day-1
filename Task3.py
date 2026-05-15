4a9d8bd5935e4a02caf78a0b192aed8d4094d783
real-hassnain <hassnainp2@gmail.com>
2026-05-15 18:56:39 +0500

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,13 @@
+a=1
+b=2
+c=3
+d=4
+e=5
+f=6
+g=7
+h=8
+i=9
+
+x=a+b+c+d+e+f+g+h+i;
+
+print("summation total", x)
---
+++ Task4.py
@@ -0,0 +1,9 @@
+radius= 5.5
+p= 3.14
+
+perimeter= 2 * radius * p
+area= radius * radius * p
+
+
+print("Perimeter of the circle:", perimeter) 
+print("Area of the circle:", area) 
